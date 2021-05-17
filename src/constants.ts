0bf23beacfbaef6d25fd58798441cd0f1af3dea0
zhutianchi <zhutianchi@mcarlo.com>
2021-05-17 20:18:55 +0800

    arbitrum kovan5

--- src/constants.ts
+++ src/constants.ts
@@ -17,6 +17,8 @@ export const CHAIN_ID_TO_READER_ADDRESS: { [chainID: number]: string } = {
   // 56: '',
   // bsc testnet
   97: '0x5c32d02948165c75F682A59aF1a9f654F3Ece214',
+  // arbitrum kovan5
+  144545313136048: '0x34Ee759Dd399F35E63d08A9A5834C148b3fC974F'
 }
 
 export const CHAIN_ID_TO_POOL_CREATOR_ADDRESS: { [chainID: number]: string } = {
@@ -24,6 +26,7 @@ export const CHAIN_ID_TO_POOL_CREATOR_ADDRESS: { [chainID: number]: string } = {
   1337: '0x30DF108a6f3ec3CAFaD55E68A3D14c0654BE626A',
   // 56: '',
   97: '0x6cadfF06B18d9AeF58A974C7073F37B622D660B0',
+  144545313136048: '0x6fd4C6D4DAA885A948eC8d218e9eaBD638296aFC'
 }
 
 export const CHAIN_ID_TO_BROKER_ADDRESS: { [chainID: number]: string } = {
@@ -31,6 +34,7 @@ export const CHAIN_ID_TO_BROKER_ADDRESS: { [chainID: number]: string } = {
   1337: '0x70afC7cD35804fF1b69D1e2cF7150e2a60D4fd31',
   // 56: '',
   97: '0xde3093172Fe85691766391547169f903812d6450',
+  144545313136048: '0x9CaDa02fC03671EA66BaAC7929Cb769214621947'
 }
 
 export const CHAIN_ID_TO_ORACLE_ROUTER_CREATOR_ADDRESS: { [chainID: number]: string } = {
@@ -38,6 +42,7 @@ export const CHAIN_ID_TO_ORACLE_ROUTER_CREATOR_ADDRESS: { [chainID: number]: str
   1337: '0xc7b1229fd865f8CC0964FeEDCE7ACa288a8E0A36',
   // 56: '',
   97: '0x8d44fd514E16c3148cEfA6b759a715d58a11e676',
+  144545313136048: '0x0A772086d8d6898e7Dce4BCB114BE74464aECBC3'
 }
 
 export const CHAIN_ID_SYMBOL_SERVICE_ADDRESS: { [chainID: number]: string } = {
@@ -45,6 +50,7 @@ export const CHAIN_ID_SYMBOL_SERVICE_ADDRESS: { [chainID: number]: string } = {
   1337: '0xAf85a1998ef007e85c95a2840cbfeE6dE9Bab48c',
   // 56: '',
   97: '0x63372427A3Bcc8A7d7F04f3A4581A37Cd41f89c4',
+  144545313136048: '0x27Ca190b32D8fe7274E84f575feF6E22BDEcA4A5'
 }
 
 // leave 1% when calculating withdrawal penalty if position != 0
